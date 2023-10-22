1294e57ab43711a9726a02c006c09ef01c9e3740
horlakz <officialhorlakz@gmail.com>
2023-10-22 19:53:16 +0100

    feat(src/pages/): adds hero page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Center, Image, Stack, Text, VStack } from "@chakra-ui/react";
+
+import hero from "@/assets/hero.png";
 
 const HomePage = (): JSX.Element => {
   return (
     <Box>
-      <Heading>Lorem, ipsum dolor.</Heading>
+      <Center w="full" px={["8", "20"]} py="10">
+        <Stack direction={["column-reverse", "row"]}>
+          <VStack w="full" alignItems="center" justifyContent="center">
+            <Text fontSize={["2xl", "5xl"]} fontWeight="medium">
+              We specialize in UI/UX, Web Development, Digital Marketing
+            </Text>
+            <Text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sunt
+              reiciendis hic laudantium dolor culpa voluptate ullam! Ratione
+              provident excepturi deleniti architecto id reprehenderit.
+            </Text>
+          </VStack>
+
+          <Center w="full">
+            <Image src={hero} />
+          </Center>
+        </Stack>
+      </Center>
     </Box>
   );
 };
